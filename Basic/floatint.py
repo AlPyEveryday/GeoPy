8242d894931c04b6ac29ee98f00b65d8ded9b4c0
sohappytoday <kgeo6264@gmail.com>
2025-07-05 15:01:49 +0900

    2nd

--- Basic/floatint.py
+++ Basic/floatint.py
@@ -3,4 +3,8 @@ print(int(3.92))
 print(3.14**2)      #제곱
 print(3.14//2)      #몫
 print(3.14/2)       #정확한 나누기
-print(3.14%2)
+print(3.14%2)       #3.14로 나눈 나머지
+
+a,b,c = map(int,input("3개의 값을 입력하세요 : ").split(' '))
+sum=a+b+c
+print("3개의 합은 "+ str(sum)+"입니다")
